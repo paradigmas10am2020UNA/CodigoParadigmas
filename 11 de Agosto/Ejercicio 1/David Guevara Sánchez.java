c53a8142b4a84ae6fe78789e6c3d83584e7b39d4
David Guevara <datwaftx@gmail.com>
2020-08-14 12:26:42 -0600

    Create David Guevara Sánchez.java

---
+++ 11 de Agosto/Ejercicio 1/David Guevara Sánchez.java
@@ -0,0 +1,5 @@
+int foo(int n) {
+  return IntStream.rangeClosed(1, n).reduce((f, x) -> f * x).orElse(1);
+}
+
+// La respuesta es factorial, ya que multiplica todos los valores en un rango y acumula dicha multiplicación.
