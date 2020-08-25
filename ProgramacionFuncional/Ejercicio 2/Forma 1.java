e35bb4def8c5cf28eace0a630056f429c0477ec3
KevinFloresG <51028866+KevinFloresG@users.noreply.github.com>
2020-08-25 10:51:46 -0600

    Add files via upload

---
+++ ProgramacionFuncional/Ejercicio 2/Forma 1.java
@@ -0,0 +1,10 @@
+<T, R>
+List<R> myMap(List<T> list, Function<T, R> f)  {
+  return list.stream()
+    .reduce(List.of(), (acc, x) -> new ArrayList<R>() {{ addAll(acc); add(f.apply(x)); }}, (x,y) -> new ArrayList<R>() {{ addAll(x); addAll(y); }});
+}
+
+// El método usa una sintaxis rara new ArrayList<R>() {{ addAll(x); addAll(y); }} para generar una nueva lista que fusione las dos listas en un solo statement.
+
+// Autor(es):
+//   David Alberto Guevara Sánchez
