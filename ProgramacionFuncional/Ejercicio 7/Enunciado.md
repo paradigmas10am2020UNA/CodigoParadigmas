1e38ce2c3ac735da76be776cbfa1cf7013844d8e
Java64x <55204353+Java64x@users.noreply.github.com>
2020-08-27 17:16:18 -0600

    Ejercicio 02 clase 25 de Agosto.
    Implementación de byProvinceAndGender(  List<Person> persons )

---
+++ ProgramacionFuncional/Ejercicio 7/Enunciado.md
@@ -0,0 +1,4 @@
+#  Haga un método  ??? byProviceAndGender(List<Person> persons) en la clase Persona.java dada.
+
+
+Modifique Persona.java para que una persona tenga un atributo province (enum con las provincias de CR). Modifique para la generación de personas genPersons(), para que se produzca ese campo aleatoriamente. Luego haga el método  ??? byProviceAndGender(List<Person> persons) una agrupación por provincia y dentro de provincia por género. Indique como parte del ejercicio cuál es el tipo ???? de retorno de byProvinceAndGender. Use colectores.
---
+++ ProgramacionFuncional/Ejercicio 7/Forma 1 (FP).java
@@ -0,0 +1,89 @@
+/**
+Demo para Collectors
+@author loriacarlos@gmail.com
+
+*/
+
+enum Gender{FEMALE, MALE}
+enum Province{ SAN_JOSE,LIMON,HEREDIA,CARTAGO,GUANACASTE,PUNTARENAS,ALAJUELA}
+
+record Person(String name, int age, Gender gender, Province province){
+    int compareTo(Person p){
+        return this.age() - p.age();
+    }
+}
+
+var rand = new Random(666);
+
+/*Método que devuelve una provincia en base a un int del 1 al 7*/
+Province getPrvcByNum( int n ){
+	
+	switch(n) {
+	  case 1:
+		return Province.SAN_JOSE;
+		
+	  case 2:
+		return Province.LIMON;
+		
+	  case 3:
+		return Province.HEREDIA;
+		
+	  case 4:
+		return Province.CARTAGO;
+		
+	  case 5:
+		return Province.GUANACASTE;
+		
+	  case 6:
+		return Province.PUNTARENAS;
+		
+	  case 7:
+		return Province.ALAJUELA;
+		
+	  default:
+		return Province.SAN_JOSE;
+	}
+	
+}
+
+List<Integer> genAges(int a, int b, int max){
+    return rand.ints(a, b).limit(max).boxed().collect(Collectors.toList());
+}
+
+List<Gender> genGenders(int max){
+    return rand.ints().limit(max)
+                          .boxed()
+                          .map(n -> n % 2 == 0 ? Gender.FEMALE : Gender.MALE)
+                          .collect(Collectors.toList());
+}
+
+/* Método que genera una lista con n cantidad de provincias utilizando el método getPrvcByNum( int n ) */
+List<Province> genProvinces(int max){
+    return rand.ints(1,8).limit(max)
+                          .boxed()
+                          .map(n -> getPrvcByNum(n))
+                          .collect(Collectors.toList());
+}
+
+List<Person> genPersons() throws Exception{
+    var names = Files.lines(Paths.get("names.txt")).collect(Collectors.toList());
+    var ages = genAges(18, 90, names.size());
+    var genders = genGenders(names.size());
+	var provinces = genProvinces(names.size());
+    return IntStream.range(0, names.size()).boxed()
+           .map( i -> new Person(names.get(i), ages.get(i), genders.get(i), provinces.get(i) )) 
+           .collect(Collectors.toList());
+}
+
+
+/*Método byProvinceAndGender que devuelve la agrupación de personas según provincia y dentro de estas se agrupan por género. 
+El tipo devuelto es Map< Province, Map< Gender, List< Person > > > */ 
+Map< Province, Map< Gender, List< Person > > > byProvinceAndGender( List<Person> persons ){
+return personas.stream().collect( Collectors.groupingBy(Person::province, Collectors.groupingBy(Person::gender) )); 
+}
+
+
+
+/* Autor(es):
+     Javier Alberto Amador Delgado
+*/
