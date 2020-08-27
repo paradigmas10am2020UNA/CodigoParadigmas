482e2f4a05da0111f08d5883cfe6370fd9f27a19
KevinFloresG <51028866+KevinFloresG@users.noreply.github.com>
2020-08-27 15:56:27 -0600

    Create Forma 1.java

---
+++ ProgramacionFuncional/Ejercicio 5/Forma 1.java
@@ -0,0 +1,28 @@
+// POJO
+record Estate( int n1, int n2, int i ){
+
+    public static Estate initial(int n){
+        return new Estate( 0, 1 , n);
+    }
+    
+    public Estate update(){
+        return new Estate( n2, n1 + n2, i - 1 );
+    }    
+    
+    public boolean isFinal(){
+        return i == 0;
+    }
+    
+}
+
+Integer fibonacci_fp( int n ){
+    return Stream.iterate( Estate.initial(n), Estate::update )
+                 .filter( Estate::isFinal )
+                 .findFirst()
+                 .get()
+                 .n1();                
+}
+
+/* Autor(es):
+    Kevin Flores Garcia
+*/
