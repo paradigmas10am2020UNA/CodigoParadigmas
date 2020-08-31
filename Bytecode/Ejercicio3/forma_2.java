161a36bf571cb75b92df9952bff40374a6b8933f
esoj1010 <56928967+esoj1010@users.noreply.github.com>
2020-08-30 20:47:14 -0600

    Add files via upload

---
+++ Bytecode/Ejercicio3/forma_2.java
@@ -0,0 +1,19 @@
+public class Main{
+	
+	public int hoo(int x, int y){
+	    int i = x;
+		while( i > 0){
+			y *= i;
+			i--;
+		}
+		return y;
+    }
+	
+    static public void main(String... args){
+		/*El metodo hoo(int x, int y) 
+		Obtiene el factorial de 'x', pero con 'y' al  inicio*/
+	   System.out.println(new Main().hoo(4,2));  // 48 --> fact(4) = 24  * 2 = 48
+    }
+}
+
+//creador: jose barrantes araya
